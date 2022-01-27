ea4460426f4d2bfedd68efb844735c2b5436cf74
Randy Osoti <rosoti17@apu.edu>
2022-01-27 13:44:47 -0800

    initial FB authentication

--- server/api-routes/auth/signup.ts
+++ server/api-routes/auth/signup.ts
@@ -1,4 +1,29 @@
 import { Router, Request, Response } from 'express';
+import passport from "passport";
+import dotenv from "dotenv";
+import strategy from "passport-facebook";
+
+const FacebookStrategy = strategy.Strategy;
+
+passport.use(
+    new FacebookStrategy(
+        {
+            clientID: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+            callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+            profileFields: ["email", "name"]
+        },
+        function (accessToken, refreshToken, profile, done) {
+            const { email, first_name, last_name } = profile._json;
+            const userData = {
+                email,
+                firstName: first_name,
+                lastName: last_name
+            };
+            done(null, profile);
+        }
+    )
+);
 
 const router = Router();
 
@@ -7,7 +32,7 @@ const router = Router();
 //     // login with google
 // });
 
-router.post('/facebook', (req: Request, res: Response) => {
+router.post('/facebook', passport.authenticate("facebook"), (req: Request, res: Response) => {
     // login with facebook
 });
 
--- server/server.ts
+++ server/server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 import signinRouter from './api-routes/auth/signin';
 import signupRouter from './api-routes/auth/signup';
 
+import passport from "passport";
+
 const app = express();
 const port = 3000;
+
+app.use(passport.initialize());
 
 // signin/signup routers
 app.use('/signin', signinRouter);
