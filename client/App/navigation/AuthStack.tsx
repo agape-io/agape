21694c03cc5898d02c6c85ee1ebe7edbbc944e15
Leonardo Tomasi <ltomasi19@apu.edu>
2022-02-02 16:15:55 -0800

    implementing landing page

--- client/App/navigation/AuthStack.tsx
+++ client/App/navigation/AuthStack.tsx
@@ -9,14 +9,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // Screens
 import { SignUp } from '../pages';
 import { SignIn } from '../pages';
+import { LandingPage } from '../pages';
 
 const Auth = createNativeStackNavigator();
 
 function AuthStack() {
   return (
-    <Auth.Navigator initialRouteName='Sign In'>
+    <Auth.Navigator initialRouteName='Landing Page'>
       <Auth.Screen name="Sign Up" component={SignUp} options={{ headerShown: false }} />
       <Auth.Screen name="Sign In" component={SignIn} options={{ headerShown: false }} />
+      <Auth.Screen name="Landing Page" component={LandingPage} options={{ headerShown: false }} />
+
     </Auth.Navigator>
   )
 }
