87aec8585351458c817824a8cbc4f0dc700620b7
Cy Bunayog <cybunayog@gmail.com>
2022-02-03 14:21:12 -0800

    setting up auth context

--- client/App/navigation/AuthProvider.tsx
+++ client/App/navigation/AuthProvider.tsx
@@ -3,3 +3,18 @@
  */
 
 //TODO: Create auth provider once backend is created
+import React, { createContext, useState } from 'react';
+
+interface IAuthContext {
+  user: string;
+  setUser: string;
+  signIn: () => void;
+  signUp: () => void;
+  signOut: () => void;
+}
+
+const defaultState = {
+
+};
+
+export const AuthContext = createContext<IAuthContext | null>(null);
