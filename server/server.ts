87f8440a8ca9926a6eb134732a7221df61f77599
Randy Osoti <rosoti17@apu.edu>
2022-02-03 13:31:40 -0800

    utilize routes in server

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,8 @@ import passport from "passport";
 import signinRouter from './api-routes/auth/signin';
 import signupRouter from './api-routes/auth/signup';
 
+import profileRouter from './api-routes/users/profile';
+
 const app = express();
 const port = 3000;
 
@@ -14,6 +16,9 @@ app.use(bodyParser.json());
 // signin/signup routers
 app.use('/signin', signinRouter);
 app.use('/signup', signupRouter);
+
+// profile routers
+app.use('/profile', profileRouter);
 
 app.listen(port, () => {
   console.log(`Agape Server is listening on port ${port}!`);
