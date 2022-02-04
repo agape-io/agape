12c2f3a49c244cef5405664f44fb06562a362947
Cy Bunayog <cybunayog@gmail.com>
2022-02-04 01:13:22 -0800

    Auth Provider is set up

--- client/App/navigation/AuthProvider.tsx
+++ client/App/navigation/AuthProvider.tsx
@@ -3,18 +3,42 @@
  */
 
 //TODO: Create auth provider once backend is created
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect, FC } from 'react';
+import axios from 'axios';
 
-interface IAuthContext {
-  user: string;
-  setUser: string;
-  signIn: () => void;
-  signUp: () => void;
-  signOut: () => void;
+interface IAuth {
+  token?: string | null;
+  signIn: (email: string, password: string) => void;
+  signUp: (email: string, password: string, verifyPassword: string) => void;
+  logout: (token: string) => void;
 }
 
-const defaultState = {
+export const AuthContext = createContext<IAuth>({} as IAuth);
 
-};
 
-export const AuthContext = createContext<IAuthContext | null>(null);
+export const AuthContextProvider: FC<IAuth> = ({ children }) => {
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        signIn: async (email, password) => {
+          // set sign in
+          console.log("sign in");
+        },
+        signUp: async (email, password, verifyPassword) => {
+          // set sign up
+          console.log('sign up');
+        },
+        logout: (token) => {
+          // remove token
+          console.log('log out')
+        }
+
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+
+}
