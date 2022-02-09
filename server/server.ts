9113e06692325d7e2025ac15026aef5a0694fba2
Randy Osoti <rosoti17@apu.edu>
2022-02-08 21:08:27 -0800

    implement discover feature

--- server/server.ts
+++ server/server.ts
@@ -7,7 +7,7 @@ import signinRouter from './api-routes/auth/signin';
 import signupRouter from './api-routes/auth/signup';
 
 import profileRouter from './api-routes/users/profile';
-import discoverRouter from './api-routes/discover/discover';
+import discoverRouter from './api-routes/users/discover';
 
 const app = express();
 const port = 3000;
@@ -22,7 +22,7 @@ app.use('/signup', signupRouter);
 // profile routers
 app.use('/profile', authenticateToken, profileRouter);
 // discover routers
-app.use('/discover', discoverRouter);
+app.use('/discover', authenticateToken, discoverRouter);
 
 app.listen(port, () => {
   console.log(`Agape Server is listening on port ${port}!`);
