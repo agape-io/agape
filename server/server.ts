06c66341869855796c88e115d0ae8f3e9d8a24f2
Randy Osoti <rosoti17@apu.edu>
2022-02-21 13:20:52 -0800

    implement settings get/create/update APIs

--- server/server.ts
+++ server/server.ts
@@ -10,6 +10,7 @@ import signoutRouter from './api-routes/auth/signout';
 
 import profileRouter from './api-routes/users/profile';
 import discoverRouter from './api-routes/users/discover';
+import settingsRouter from './api-routes/users/settings';
 
 const app = express();
 const port = 3000;
@@ -29,6 +30,8 @@ app.use('/signout', signoutRouter);
 app.use('/profile', authenticateToken, profileRouter);
 // discover routers
 app.use('/discover', authenticateToken, discoverRouter);
+// settings routers
+app.use('/settings', authenticateToken, settingsRouter);
 
 app.listen(port, () => {
   console.log(`Agape Server is listening on port ${port}!`);
