c4a3f714b7231a6de09bd2aedd96ca93dbd224cf
Cy Bunayog <cybunayog@gmail.com>
2022-03-02 13:50:14 -0800

    adding modal boilerplate

---
+++ client/App/components/Modal.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import {
+  Modal,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+// API's
+import { getProfile, createProfile } from '../utils';
+
+//  https://www.reactnativeschool.com/how-to-upload-images-from-react-native
+
+const ProfileModal: FC<any> = ({ navigation }) => {
+  
+  return null;
+}
+
+export default ProfileModal;
+
