8e46be93549f0ef070ddad6e757d8f258851d4f0
Randy Osoti <rosoti17@apu.edu>
2022-04-12 16:54:36 -0700

    lint subscription APIs

--- server/api-routes/users/subscription.ts
+++ server/api-routes/users/subscription.ts
@@ -4,7 +4,9 @@ import moment from 'moment';
 import { DATE_FORMAT } from '../../config/constants';
 import connect from '../../config/db';
 import { MISSING_FIELDS, UNKNOWN_ERROR } from '../../config/errorMessages';
-import { SUBSCRIPTION_GET_PLAN_SUCCESS, SUBSCRIPTION_GET_PLANS_SUCCESS, USER_PLAN_CANCELLATION, USER_PLAN_SUBSCRIPTION } from '../../config/statusMessages';
+import {
+  SUBSCRIPTION_GET_PLAN_SUCCESS, SUBSCRIPTION_GET_PLANS_SUCCESS, USER_PLAN_CANCELLATION, USER_PLAN_SUBSCRIPTION,
+} from '../../config/statusMessages';
 
 import { User } from '../../models/user';
 import { Plan } from '../../models/plan';
@@ -29,7 +31,7 @@ router.get('/', (req: Request, res: Response) => {
     .then((plans: any[]) => res.status(200).send({
       status: 200,
       plans,
-      message: SUBSCRIPTION_GET_PLANS_SUCCESS
+      message: SUBSCRIPTION_GET_PLANS_SUCCESS,
     }))
     .catch((err: any) => {
       console.error(err);
@@ -37,7 +39,7 @@ router.get('/', (req: Request, res: Response) => {
         status: 500,
         message: UNKNOWN_ERROR,
       });
-    })
+    });
 });
 
 /**
@@ -59,27 +61,27 @@ router.get('/myPlan', (req: Request, res: Response) => {
   const { userId } = req.query;
   if (userId) {
     connect()
-    .then(() => User.findOne({
-      _id: userId,
-    }).populate('settings.membershipType', '-createdAt -updatedAt -__v'))
-    .then((user: any) => {
-      const { settings } = user;
-      let endingDate = null;
-      if (settings.endingDate != null) endingDate = moment(settings.endingDate).format(DATE_FORMAT);
-      res.status(200).send({
-        status: 200,
-        message: SUBSCRIPTION_GET_PLAN_SUCCESS,
-        subscription: settings.membershipType,
-        endingDate,
+      .then(() => User.findOne({
+        _id: userId,
+      }).populate('settings.membershipType', '-createdAt -updatedAt -__v'))
+      .then((user: any) => {
+        const { settings } = user;
+        let endingDate = null;
+        if (settings.endingDate != null) endingDate = moment(settings.endingDate).format(DATE_FORMAT);
+        res.status(200).send({
+          status: 200,
+          message: SUBSCRIPTION_GET_PLAN_SUCCESS,
+          subscription: settings.membershipType,
+          endingDate,
+        });
+      })
+      .catch((err: any) => {
+        console.error(err);
+        res.status(500).send({
+          status: 500,
+          message: UNKNOWN_ERROR,
+        });
       });
-    })
-    .catch((err: any) => {
-      console.error(err);
-      res.status(500).send({
-        status: 500,
-        message: UNKNOWN_ERROR,
-      });
-    })
   } else {
     res.status(400).send({
       status: 400,
@@ -107,33 +109,34 @@ router.post('/subscribe', (req: Request, res: Response) => {
   const { userId, planId } = req.body;
   if (userId && planId) {
     connect()
-    .then(() => {
-      const date = new Date();
-      const nextMonthDate = date.setMonth(date.getMonth() + 1);
-      const settings = {
-        membershipType: planId,
-        endingDate: nextMonthDate,
-        billingDate: nextMonthDate,
-      };
-      return User.findByIdAndUpdate(
-        { _id: userId },
-        {
-          $set: {
-            settings,
+      .then(() => {
+        const date = new Date();
+        const nextMonthDate = date.setMonth(date.getMonth() + 1);
+        const settings = {
+          membershipType: planId,
+          endingDate: nextMonthDate,
+          billingDate: nextMonthDate,
+        };
+        return User.findByIdAndUpdate(
+          { _id: userId },
+          {
+            $set: {
+              settings,
+            },
           },
-        }
-    )})
-    .then(() => res.status(200).send({
-      status: 200,
-      message: USER_PLAN_SUBSCRIPTION
-    }))
-    .catch((err: any) => {
-      console.error(err);
-      res.status(500).send({
-        status: 500,
-        message: UNKNOWN_ERROR,
+        );
+      })
+      .then(() => res.status(200).send({
+        status: 200,
+        message: USER_PLAN_SUBSCRIPTION,
+      }))
+      .catch((err: any) => {
+        console.error(err);
+        res.status(500).send({
+          status: 500,
+          message: UNKNOWN_ERROR,
+        });
       });
-    })
   } else {
     res.status(400).send({
       status: 400,
@@ -161,25 +164,25 @@ router.post('/cancel', (req: Request, res: Response) => {
   const { userId } = req.body;
   if (userId) {
     connect()
-    .then(() => User.findById({
-      _id: userId,
-    }))
-    .then((user: any) => {
-      const { settings } = user;
-      settings.billingDate = null;
-      return user.save();
-    })
-    .then(() => res.status(201).send({
-      status: 201,
-      message: USER_PLAN_CANCELLATION,
-    }))
-    .catch((err: any) => {
-      console.error(err);
-      res.status(500).send({
-        status: 500,
-        message: UNKNOWN_ERROR,
+      .then(() => User.findById({
+        _id: userId,
+      }))
+      .then((user: any) => {
+        const { settings } = user;
+        settings.billingDate = null;
+        return user.save();
+      })
+      .then(() => res.status(201).send({
+        status: 201,
+        message: USER_PLAN_CANCELLATION,
+      }))
+      .catch((err: any) => {
+        console.error(err);
+        res.status(500).send({
+          status: 500,
+          message: UNKNOWN_ERROR,
+        });
       });
-    })
   } else {
     res.status(400).send({
       status: 400,
